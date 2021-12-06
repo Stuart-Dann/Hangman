fa08a70777b4ba38eac99e9ed6ceb7001a4d5ead
Thrgamer <stuartdann03@gmail.com>
2021-12-06 10:54:28 +0000

    06/12/21

--- Hangman.js
+++ Hangman.js
@@ -53,7 +53,7 @@ const wordList = [
 ];
 
 const randomWord = (words) => words[Math.floor(Math.random() * words.length + 1)];
-const word = randomWord(wordList);
+const word = 'riddle';
 // randomWord(wordList)
 let domArray = [];
 let guesses = [];
@@ -62,13 +62,23 @@ for (let i = 0; i < word.length; i++) {
 	domArray.push('_');
 }
 
-const setUp = () => {
+const setup = () => {
 	document.getElementById('domWord').textContent = domArray.join(' ');
 };
+document.addEventListener('keyup', keyID);
 
-function checkGuess() {
-	const guess = document.getElementById('letter').value;
+window.addEventListener('load', setup);
+
+function keyID(e) {
+	let key = e.code;
+	key = key.substr(3).toLowerCase();
+	checkGuess(key);
+	console.log(key);
+}
+
+function checkGuess(guess) {
 	if (word.includes(guess)) {
+		document.getElementById('status').textContent = 'Correct!';
 		let indices = [];
 		for (let i = 0; i < word.length; i++) {
 			if (word[i] === guess) indices.push(i);
@@ -77,7 +87,18 @@ function checkGuess() {
 			domArray[indices[i]] = word[indices[i]];
 		}
 		guesses.push(guess);
+	} else {
+		if (guesses.includes(guess)) {
+			document.getElementById('status').textContent = "You've already guessed that!";
+		} else {
+			document.getElementById('status').textContent = 'Incorrect!';
+			lives -= 1;
+			if (lives <= 0) {
+				document.getElementById('status').textContent = 'You Lose!';
+			}
+			document.getElementById('guessed').textContent += guess + ' ';
+			guesses.push(guess);
+		}
 	}
-	guesses.push(guess);
 	setUp();
 }
