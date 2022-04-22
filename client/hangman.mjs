e083084eec7d6c6d978181b967564c7dbb1197c4
Thrgamer <stuartdann03@gmail.com>
2022-04-22 16:55:54 +0100

    DONE!

--- client/hangman.mjs
+++ client/hangman.mjs
@@ -1,61 +1,8 @@
 // const prompt = require('prompt-sync')({ sigint: true });
 // import e from 'express';
 // import { response } from 'express';
-import { draw } from './draw.mjs';
-import { home } from './home.mjs';
-
-// const wordList = [
-// 	'consider',
-// 	'occur',
-// 	'gentle',
-// 	'crush',
-// 	'tasteful',
-// 	'measure',
-// 	'border',
-// 	'moldy',
-// 	'rustic',
-// 	'dust',
-// 	'boundless',
-// 	'aback',
-// 	'stale',
-// 	'deserve',
-// 	'squeak',
-// 	'splendid',
-// 	'side',
-// 	'friend',
-// 	'punishment',
-// 	'colorful',
-// 	'ring',
-// 	'march',
-// 	'concerned',
-// 	'gleaming',
-// 	'inconclusive',
-// 	'tire',
-// 	'brief',
-// 	'zealous',
-// 	'wool',
-// 	'telephone',
-// 	'include',
-// 	'gate',
-// 	'automatic',
-// 	'library',
-// 	'embarrassed',
-// 	'beneficial',
-// 	'tiger',
-// 	'rabbit',
-// 	'chemical',
-// 	'swing',
-// 	'shiver',
-// 	'actor',
-// 	'riddle',
-// 	'hospitable',
-// 	'deserted',
-// 	'hallowed',
-// 	'lacking',
-// 	'advice',
-// 	'wax',
-// 	'loaf',
-// ];
+import { draw } from "./draw.mjs";
+import { home } from "./home.mjs";
 
 // const randomWord = (words) => words[Math.floor(Math.random() * words.length)];
 let lives = 9;
@@ -63,99 +10,128 @@ let word;
 let shownWord = [];
 let guesses = [];
 let local;
+let gameOver = false;
 
 async function loadWord() {
-	const response = await fetch('words');
+	const response = await fetch("words");
 	word = await response.json();
+	shownWord = word.shownWord;
+	document.querySelector("#domWord").textContent = shownWord.join(" ");
+	console.log(word.word);
 }
 
-async function loadGuess(guess) {
-	const payload = { msg: guess };
+async function loadGuess(guess, shownWord) {
+	let payload = { msg: guess, word: word.word, shownWord: shownWord, gameOver: gameOver };
 
-	const response = await fetch('guess', {
-		method: 'POST',
-		headers: { 'Content-Type': 'application/json' },
+	const response = await fetch("words", {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
 		body: JSON.stringify(payload),
 	});
 
 	if (response.ok) {
 		const svrGuess = await response.json();
-		console.log(svrGuess);
+		console.log(svrGuess.guess, svrGuess.correct, svrGuess.guessed);
+		shownWord = svrGuess.shownWord;
+		console.log();
+		responseToServer(svrGuess.shownWord, svrGuess.correct, svrGuess.guessed, svrGuess.guess);
+	} else {
+		console.log("Error");
 	}
 }
 
-function setup(local) {
-	if (document.querySelector('#screen-cover')) {
-		document.querySelector('#screen-cover').remove();
+function responseToServer(shownWord, correct, guessed, guess) {
+	const domword = document.querySelector("#domWord");
+	domword.textContent = shownWord.join(" ");
+	if (!guessed) {
+		document.querySelector("#guessed").textContent += guess + " ";
+		lives -= 1;
+		draw(lives);
+		if (lives <= 0) {
+			endScreen(false);
+		}
+		if (correct) {
+			document.querySelector("#status").textContent = "Correct";
+		} else {
+			document.querySelector("#status").textContent = "Incorrect";
+		}
+		if (!shownWord.includes("_")) {
+			endScreen(true);
+		}
+	} else {
+		document.querySelector("#status").textContent = `You have already guessed ${guess}!`;
+	}
+}
+
+function setup() {
+	if (document.querySelector("#screen-cover")) {
+		document.querySelector("#screen-cover").remove();
 	}
 	toggleDisableKeys(false);
-	document.querySelector('#status').textContent = 'Click a button to start guessing...';
-	document.querySelector('#guessed').textContent = 'Letters Guessed:';
+	document.querySelector("#status").textContent = "Click a button to start guessing...";
+	document.querySelector("#guessed").textContent = "Letters Guessed:";
 	lives = 9;
 	//If Vs local is selected
 	if (local == true) {
-		document.body.removeEventListener('keyup', keyupEvent);
+		document.body.removeEventListener("keyup", keyupEvent);
 
 		const cover = createCover();
 
-		const text = document.createElement('h1');
-		text.id = 'localH1';
-		text.textContent = 'Enter a Word:';
+		const text = document.createElement("h1");
+		text.id = "localH1";
+		text.textContent = "Enter a Word:";
 		cover.prepend(text);
 
-		const input = document.createElement('input');
-		input.type = 'text';
-		input.id = 'local-word';
+		const input = document.createElement("input");
+		input.type = "text";
+		input.id = "local-word";
 		cover.append(input);
 
-		const button = document.createElement('button');
-		button.textContent = 'Submit';
-		button.addEventListener('click', () => {
-			word = document.querySelector('#local-word').value;
+		const button = document.createElement("button");
+		button.textContent = "Submit";
+		button.addEventListener("click", () => {
+			word = document.querySelector("#local-word").value;
 			shownWord = [];
 			guesses = [];
 			for (let i = 0; i < word.length; i++) {
-				shownWord.push('_');
+				shownWord.push("_");
 			}
-			document.querySelector('#domWord').textContent = shownWord.join(' ');
-			document.querySelector('#screen-cover').remove();
-			document.body.addEventListener('keyup', keyupEvent);
+			document.querySelector("#domWord").textContent = shownWord.join(" ");
+			document.querySelector("#screen-cover").remove();
+			document.body.addEventListener("keyup", keyupEvent);
 		});
 		cover.append(button);
 	} else {
+		gameOver = false;
 		loadWord();
-		console.log(word);
-		shownWord = [];
-		guesses = [];
-
-		document.querySelector('#domWord').textContent = shownWord.join(' ');
-		document.body.addEventListener('keyup', keyupEvent);
+		document.body.addEventListener("keyup", keyupEvent);
 	}
 	draw(lives);
 }
 
 function bttnAttcher() {
-	const keys = document.querySelectorAll('.key');
+	const keys = document.querySelectorAll(".key");
 	for (const key of keys) {
-		key.addEventListener('click', buttonEvent);
+		key.addEventListener("click", buttonEvent);
 	}
-	const reset = document.querySelector('#reset-button');
-	reset.addEventListener('click', setup);
+	const reset = document.querySelector("#reset-button");
+	console.log(local);
+	reset.addEventListener("click", setup);
 }
 
 function toggleDisableKeys(bool) {
-	const keys = document.querySelectorAll('.key');
+	const keys = document.querySelectorAll(".key");
 	for (const key of keys) {
 		key.disabled = bool;
 	}
 }
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
 	home();
 });
 
 export function hangmanloaded(e) {
-	if (e == 'Vs Local') {
+	if (e == "Vs Local") {
 		local = true;
 	} else {
 		local = false;
@@ -167,94 +143,107 @@ export function hangmanloaded(e) {
 
 //make the on screen keyboard when page is loaded
 function makeKeyboard() {
-	const keyboard = document.querySelector('#keyboard');
-	const alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	const keyboard = document.querySelector("#keyboard");
+	const alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	for (const letter of alpha) {
-		const elem = document.createElement('button');
-		elem.classList.add('key');
+		const elem = document.createElement("button");
+		elem.classList.add("key");
 		elem.textContent = letter;
 		keyboard.append(elem);
 	}
 }
 function keyupEvent(e) {
-	const keys = document.querySelectorAll('.key');
+	const keys = document.querySelectorAll(".key");
 	for (const key of keys) {
 		if (key.textContent == e.key.toUpperCase()) {
 			key.disabled = true;
 		}
 	}
-	// checkGuess(e.key);
-	loadGuess(e.key);
+	console.log(local);
+	if (local) {
+		checkGuess(e.key);
+	} else {
+		loadGuess(e.key, shownWord);
+	}
 }
 
 function buttonEvent(e) {
 	e.target.disabled = true;
-	checkGuess(e.target.textContent.toLowerCase());
+	if (local) {
+		checkGuess(e.target.textContent.toLowerCase());
+	} else {
+		loadGuess(e.target.textContent.toLowerCase(), shownWord);
+	}
 }
 
 function checkGuess(letter) {
 	//Check if a key from the computer keyboard has been clicked instead of the on screen
-	const status = document.querySelector('#status');
+	const status = document.querySelector("#status");
 	if (guesses.includes(letter)) {
 		status.textContent = `You have already guessed ${letter}!`;
 		return;
 	}
 	if (word.includes(letter)) {
-		status.textContent = 'Correct!';
+		status.textContent = "Correct!";
 		for (let i = 0; i < word.length; i++) {
 			if (word[i] === letter) shownWord[i] = letter;
 		}
 		guesses.push(letter);
 	} else {
-		status.textContent = 'Incorrect!';
+		status.textContent = "Incorrect!";
 		lives -= 1;
 		draw(lives);
 		if (lives <= 0) {
-			status.textContent = 'You Lose!';
+			status.textContent = "You Lose!";
 			toggleDisableKeys(true);
 			endScreen(false);
 		}
-		document.querySelector('#guessed').textContent += letter + ' ';
+		document.querySelector("#guessed").textContent += letter + " ";
 		guesses.push(letter);
 	}
-	if (!shownWord.includes('_')) {
-		status.textContent = 'You Win!';
+	if (!shownWord.includes("_")) {
+		status.textContent = "You Win!";
 		toggleDisableKeys(true);
 		endScreen(true);
 	}
-	document.querySelector('#domWord').textContent = shownWord.join(' ');
+	document.querySelector("#domWord").textContent = shownWord.join(" ");
 }
 
 function endScreen(win) {
-	document.body.removeEventListener('keyup', keyupEvent);
+	document.body.removeEventListener("keyup", keyupEvent);
 	const cover = createCover();
 	if (win) {
-		const text = document.createElement('h1');
-		text.textContent = 'You win!';
+		const text = document.createElement("h1");
+		text.textContent = "You win!";
 		cover.prepend(text);
 	} else {
-		const text = document.createElement('h1');
-		text.textContent = 'You Lose!';
+		const text = document.createElement("h1");
+		text.textContent = "You Lose!";
 		cover.prepend(text);
 	}
-	const button = document.createElement('button');
-	button.id = 'reset';
-	button.textContent = 'New Game';
-	button.style.margin = 'auto';
-	button.style.fontSize = '2em';
+	const button = document.createElement("button");
+	button.id = "reset";
+	button.textContent = "New Game";
+	button.style.margin = "auto";
+	button.style.fontSize = "2em";
 	cover.append(button);
-	button.addEventListener('click', setup);
+	button.addEventListener("click", setup);
 
-	const text = document.createElement('p');
-	text.id = 'word-reveal';
-	text.textContent = word;
+	const text = document.createElement("p");
+	text.id = "word-reveal";
+	if (local) {
+		text.textContent = word;
+	} else {
+		text.textContent = word.word;
+	}
 	cover.append(text);
 }
 
 function createCover() {
-	const cover = document.createElement('div');
-	cover.id = 'screen-cover';
-	cover.classList.toggle('fade');
+	const cover = document.createElement("div");
+	cover.id = "screen-cover";
+	cover.classList.toggle("fade");
 	document.body.prepend(cover);
 	return cover;
+	gameOver = true;
 }
--- svr.js
+++ svr.js
@@ -1,22 +1,49 @@
-import express from 'express';
-import randomWords from 'random-words';
+import express from "express";
+import randomWords from "random-words";
 
 const app = express();
-app.use(express.static('client'));
-app.listen(8080);
+app.use(express.static("client"));
 
-let shownWord;
+let shownWord = [];
+let guess;
+let guesses = [];
+let guessed = false;
+let correct;
+let word;
+let gameOver;
 
-app.get('/words', (req, res) => {
+app.get("/words", (req, res) => {
 	let word = randomWords();
+	shownWord = [];
+	guesses = [];
 	for (let i = 0; i < word.length; i++) {
-		shownWord.push('_');
+		shownWord.push("_");
 	}
-	res.json(word);
-	res.json(shownWord);
+	res.json({ word: word, shownWord: shownWord });
 });
 
-app.post('/guess', express.json(), (req, res) => {
+app.post("/words", express.json(), (req, res) => {
 	guess = req.body.msg;
-	res.json(guess);
+	word = req.body.word;
+	gameOver = req.body.gameOver;
+	if (guesses.includes(guess)) {
+		guessed = true;
+	} else if (word.includes(guess)) {
+		correct = true;
+		guessed = false;
+		guesses.push(guess);
+		for (let i = 0; i < word.length; i++) {
+			if (word[i] === guess) shownWord[i] = guess;
+		}
+	} else {
+		guessed = false;
+		correct = false;
+		guesses.push(guess);
+	}
+	if (gameOver) {
+		guesses = [];
+	}
+	res.json({ shownWord: shownWord, guess: guess, guessed: guessed, correct: correct });
 });
+
+app.listen(8080);
